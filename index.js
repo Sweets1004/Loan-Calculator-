223ba8dfc239d72b6f74a7b1739e07e082daa9b2
Sweets1004 <maggiect1008@gmail.com>
2025-04-12 17:06:51 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import LoanCalculator from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LoanCalculator />
+  </React.StrictMode>
+);
